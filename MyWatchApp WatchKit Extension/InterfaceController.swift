556c6241507ca2932a5f7ccd3d92b2050643d37a
CAH-csubramani <subramani@vir-Mob.local>
2016-05-03 18:14:25 +0530

    upload my app

---
+++ MyWatchApp WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  MyWatchApp WatchKit Extension
+//
+//  Created by subramani on 4/27/16.
+//  Copyright © 2016 virtusa. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
